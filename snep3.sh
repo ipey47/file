67c899ff1ca8fb278707be6a8313adcce4223867
ipey47 <62617498+ipey47@users.noreply.github.com>
2020-03-25 02:26:45 +0700

    Add files via upload

---
+++ snep3.sh
@@ -0,0 +1,12 @@
+ # 10 AKUN AKTIF 
+      
+      python main.py +15403900258 doge&&
+      python main.py +15303580533 ltc&&
+      python main.py +15104477835 zec&&
+      python main.py +15104801335 doge&&
+      python main.py +16503095445 ltc&&
+      sleep 10800
+      sh bot.sh
+      python main.py +15403xx0258 zec&&
+      sleep 10800
+      sh bot.sh
